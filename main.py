a2e0fcecef0a850072193ae25aa52a80e849adf3
taisey <taisey02@gmail.com>
2022-07-11 14:08:30 +0900

    コメントアウトを追加

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import module.blockchain as bc
 
 def test():
+	#ブロックを作成
 	myBlock = bc.Block()
 	myBlock.log = "hello world"
 
+
 	myBC = bc.BlockChain(myBlock)
+	
+	#ブロックをマイニング 
 	myBC.mining()
-	print("mining run:", myBC.block.pow)
+
+	print("POW:", myBC.block.pow)
+
+	#POWが正しいか確認
 	myBC.validation()
 	print("validation:", myBC.validation())
 
--- module/blockchain.py
+++ module/blockchain.py
@@ -1,18 +1,18 @@
 import hashlib
 
+DEBUG = 0
 class Block:
 	def __init__(self):
-		print("コンストラクタ")
 		self.id = 1
 		self.log = ""
 		#Proof of Work
-		self.pow = 0
+		self.pow = "Not Registerd"
 
 class BlockChain(Block):
 	def __init__(self, block):
-		self.digit = 1
+		self.digit = 3
 		self.block = block
-		self.mining_limit = 1000
+		self.mining_limit = 100000
 	
 	def sha256(self, target):
 		target_hash = str(hashlib.sha256(target.encode('utf-8')).hexdigest())
@@ -31,6 +31,8 @@ class BlockChain(Block):
 		mining_limit = self.mining_limit
 		for i in range(mining_limit):
 			target_hash = self.make_target_hash(id, log, i)
+			if(DEBUG):
+				print("target_hash {:<3}: {}".format(i, target_hash))
 			if(self.check_hash(target_hash)):
 				return i
 		
@@ -39,7 +41,7 @@ class BlockChain(Block):
 		id = block.id
 		log = block.log
 		pow = self.make_pow(id, log)
-		self.pow = pow
+		block.pow = pow
 	
 	def validation(self):
 		block = self.block
